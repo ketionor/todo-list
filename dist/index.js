bc983a68752c4c2e8577bbc50427551619ac7e9d
Josh <joshua.sandefer@gmail.com>
2021-01-16 15:13:23 -0600

    added project factory function

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,9 @@
 let today = ["Something I need to do", "Something else I need to do", "One more", "Seriously last thing"];
+let projectList = ["Project one"]
 let activePage = "Today";
 
+
+//generates the selected list onto the right panel
 const displayList = (array) => {
     for (i=0; i<array.length; i++) {
         let rightPanel = document.querySelector('.right-panel');
@@ -17,4 +20,16 @@ const displayList = (array) => {
     }
 }
 
-displayList(today);
+const projectFactory = (name) => {
+    projectName: name;
+    function addToProjectList(name) {
+        projectList.push(name);
+    }
+    addToProjectList(name);
+    return {name};
+}
+
+const fooBar = projectFactory('fooBar');
+fooBar.list = ["One","Two","Three"]
+console.log(fooBar.list);
+
